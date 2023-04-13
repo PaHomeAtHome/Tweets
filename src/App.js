852a8f31ec2ca568ac75a6057be6cf37e79cc218
Andrey Pakhomov <pahomeathome@gmail.com>
2023-04-13 04:45:10 +0300

    fetch added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const fetchUserData = () => {
+    fetch("https://6436fa4b3e4d2b4a12e09fd0.mockapi.io/api/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const [users, setUsers] = useState([]);
+
   return (
     <div className="App">
-      <p>Test</p>
+      {users.length > 0 && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.user}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
